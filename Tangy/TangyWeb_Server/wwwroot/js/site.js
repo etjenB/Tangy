7f31dd00d71323726676f5beed2fc1e605ceaf71
Etjen Buljubašić <buljubasicetjen4@gmail.com>
2024-02-11 12:48:19 +0100

    server - blazor js

---
+++ Tangy/TangyWeb_Server/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, 'Uspješna akcija', { timeOut: 5000 });
+    }
+    if (type === "error") {
+        toastr.error(message, 'Neuspješna akcija', { timeOut: 5000 });
+    }
+}
