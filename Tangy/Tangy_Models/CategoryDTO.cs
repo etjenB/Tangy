cf063258aa428581d91e71229769c5fddff77fbe
Etjen Buljubašić <buljubasicetjen4@gmail.com>
2024-02-11 22:38:46 +0100

    server - category logic list create update

--- Tangy/Tangy_Models/CategoryDTO.cs
+++ Tangy/Tangy_Models/CategoryDTO.cs
@@ -1,9 +1,11 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace Tangy_Models
 {
     public class CategoryDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Ime je obavezno")]
         public string Name { get; set; }
     }
 }
